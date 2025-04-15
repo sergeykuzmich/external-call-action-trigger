bede6c80ca1b7481df03ee61d97d6dbd3b267a4b
Sergey Kuzmich <accounts@kuzmi.ch>
2025-04-15 14:07:26 +0200

    Use payload variable for remote call

--- cypress/e2e/call.cy.js
+++ cypress/e2e/call.cy.js
@@ -2,6 +2,6 @@ describe("behaves as lambda", () => {
   it("execution", () => {
     cy.visit("https://example.cypress.io");
     cy.contains("wait").click();
-    cy.get(".wait-input1").type("Hello World!");
+    cy.get(".wait-input1").type(Cypress.env("MESSAGE"));
   });
 });
